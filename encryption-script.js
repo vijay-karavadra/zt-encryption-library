60b351dc9056916bcfb7f43b2cf80061562eb6cc
Vijay Karavadra <vijaykaravadra5@gmail.com>
2023-08-02 16:06:12 +0530

    Added logics

--- encryption-script.js
+++ encryption-script.js
@@ -1,5 +1,5 @@
 // encryption-library.js
-(async function() {
+
   async function encryptData(data) {
     // Implement your encryption logic here (e.g., using secure libraries and shared secret)
     // For demonstration purposes, we'll use a simple Base64 encoding
@@ -10,36 +10,42 @@
 	return data;
   }
 
-  function interceptXHRRequests() {
-    let open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-      this._url = url;
-      open.call(this, method, url, async, user, password);
-    };
+function interceptXHRRequests() {
+  let open = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+    this._url = url;
+    this._method = method; // Store the request method in _method property
+    open.call(this, method, url, async, user, password);
+  };
 
-    let send = XMLHttpRequest.prototype.send;
-    XMLHttpRequest.prototype.send = function(data) {
-      if (this.method === 'POST' && this.setRequestHeader) {
-        // Encrypt request data before sending to the API
-        let requestData = JSON.parse(data);
-        encryptData(JSON.stringify(requestData))
-          .then((encryptedData) => {
-            // Replace the original request body with the encrypted data
-			console.log('logger:encryption-library, encrypted data: '+ encryptedData);
-            send.call(this, encryptedData);
-          })
-          .catch((error) => {
-            console.error(error);
+  let send = XMLHttpRequest.prototype.send;
+  XMLHttpRequest.prototype.send = function(data) {
+    console.log('intercepted the request.');
 
-            // Call the original 'send' function with the original data
-            send.call(this, data);
-          });
-      } else {
-        // Call the original 'send' function for other types of requests
-        send.call(this, data);
-      }
-    };
-  }
+    if (this._method === 'POST') { // Check the _method property for POST
+      console.log('inside if post.');
 
-  interceptXHRRequests();
-})();
+      // Encrypt request data before sending to the API
+      let requestData = JSON.parse(data);
+      encryptData(JSON.stringify(requestData))
+        .then((encryptedData) => {
+          // Replace the original request body with the encrypted data
+          console.log('logger:encryption-library, encrypted data: ' + encryptedData);
+          send.call(this, encryptedData);
+        })
+        .catch((error) => {
+          console.error(error);
+
+          // Call the original 'send' function with the original data
+          send.call(this, data);
+        });
+    } else {
+      // Call the original 'send' function for other types of requests
+      send.call(this, data);
+    }
+  };
+}
+
+interceptXHRRequests();
+
+console.log('zt encryption script loaded.');
