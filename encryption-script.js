aa7202e32cfaa79a5dd3fb3b73843439746a0bcc
Vijay Karavadra <vijaykaravadra5@gmail.com>
2023-08-02 20:02:33 +0530

    Added encryption

--- encryption-script.js
+++ encryption-script.js
@@ -34,6 +34,45 @@ async function encryptData(data) {
   return encryptedData;
 }
 
+
+async function decryptData(data) {
+  // Implement your decryption logic here
+  // For demonstration purposes, we'll use the reverse Caesar cipher decryption
+  let decryptedData = "";
+  let shift = 3; // The number of positions to shift each character
+
+  for (let i = 0; i < data.length; i++) {
+    let char = data[i];
+
+    // Shift only alphabetic characters (uppercase and lowercase)
+    if (/[a-zA-Z]/.test(char)) {
+      let code = char.charCodeAt(0);
+      let shiftedCode = code - shift;
+
+      // Handle wrap-around for lowercase letters
+      if (char >= 'a' && char <= 'z' && shiftedCode < 'a'.charCodeAt(0)) {
+        shiftedCode += 26;
+      }
+
+      // Handle wrap-around for uppercase letters
+      if (char >= 'A' && char <= 'Z' && shiftedCode < 'A'.charCodeAt(0)) {
+        shiftedCode += 26;
+      }
+
+      decryptedData += String.fromCharCode(shiftedCode);
+    } else {
+      // Leave non-alphabetic characters unchanged
+      decryptedData += char;
+    }
+  }
+
+  return decryptedData;
+}
+
+
+
+// ... Your encryptData and decryptData functions ...
+
 function interceptXHRRequests() {
   let open = XMLHttpRequest.prototype.open;
   XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
@@ -51,14 +90,19 @@ function interceptXHRRequests() {
 
       // Encrypt request data before sending to the API
       let requestData = JSON.parse(data);
+
+	  console.log('Request, before encryption:');
+	  console.log(requestData);
+	  
       encryptData(JSON.stringify(requestData))
         .then((encryptedData) => {
-          // Replace the original request body with the encrypted data
-          console.log('logger:encryption-library, encrypted data: ' + encryptedData);
-          send.call(this, encryptedData);
-
           // Set a custom header for the encrypted data
           this.setRequestHeader('X-Encrypted-Data', 'true');
+
+		  console.log('Request, after encryption:');
+          // Replace the original request body with the encrypted data
+          console.log(encryptedData);
+          send.call(this, encryptedData);
         })
         .catch((error) => {
           console.error(error);
@@ -71,8 +115,40 @@ function interceptXHRRequests() {
       send.call(this, data);
     }
   };
-}
 
+  // Create a wrapper function to handle the response
+  function handleResponse() {
+    if (this.readyState === XMLHttpRequest.DONE && this.getResponseHeader('content-type')?.startsWith('application/json')
+		//&&
+        //this.getRequestHeader('X-Encrypted-Data') === 'true'
+		) {
+		
+	  console.log('success');
+		
+	  console.log('Response, before decryption:');
+      console.log(this.responseText);
+
+      // Decrypt the response data if it's not null or undefined
+      let decryptedResponse = this.responseText ? decryptData(this.responseText) : null;
+
+      // Replace the original response data with the decrypted data
+      this.responseText = decryptedResponse;
+		
+	  
+
+      // Log the decrypted response data (optional, for debugging purposes)
+      console.log('Response, after decryption:');
+      console.log(decryptedResponse);
+    }
+  }
+
+  // Listen for the 'load' event to handle the response
+  let originalOpen = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+    this.addEventListener('load', handleResponse); // Add the event listener for 'load' event
+    originalOpen.call(this, method, url, async, user, password);
+  };
+}
 
 interceptXHRRequests();
 
